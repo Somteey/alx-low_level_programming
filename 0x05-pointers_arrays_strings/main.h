b59707798e3d9f5c31cf4ff3f704b57a1ccf8ebf
somteey <kainetonna@gmail.com>
2022-12-19 16:15:06 -0800

    task 4

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -15,4 +17,4 @@ int _atoi(char *s);
 int _putchar(char c);
 
 
-#endif
+#endif /* MAIN_H */
